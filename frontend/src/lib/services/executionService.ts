a14de67623d0d6633742a8de57a0513976dfc7ce
tanxingzhong <zetter358@gmail.com>
2025-11-13 00:43:23 +0800

    update: deployment

--- frontend/src/lib/services/executionService.ts
+++ frontend/src/lib/services/executionService.ts
@@ -4,15 +4,18 @@ export async function runCodeApi(
   input: any,
   timeout?: number
 ) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/execute/run`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      language: language,
-      code: code,
-      input: input,
-    }),
-  });
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/execute/execute/run`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        language: language,
+        code: code,
+        input: input,
+      }),
+    }
+  );
   const data = await response.json();
   return { response: response, data: data };
 }
