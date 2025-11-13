b2b5ef27ee0477542e8e1b6350977ae21859d62b
tanxingzhong <zetter358@gmail.com>
2025-11-14 00:14:11 +0800

    update: deployment4

--- frontend/src/hooks/useSocket/constants.ts
+++ frontend/src/hooks/useSocket/constants.ts
@@ -1,3 +1,7 @@
 export const NOTIFICATION_SERVICE_URL =
+<<<<<<< Updated upstream
   "https://d3lspwis800qk7.cloudfront.net/";
+=======
+  "http://notification-env2.eba-pidmimwp.ap-southeast-1.elasticbeanstalk.com/";
+>>>>>>> Stashed changes
 export const DEFAULT_COUNTDOWN = 10;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -17,7 +17,7 @@ const HomePage: React.FC = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
   const isAdmin = !!user?.isAdmin;
-
+  const GATEWAY_URL = import.meta.env.VITE_API_URL;
   const {
     pendingMatch,
     countdown,
@@ -48,8 +48,14 @@ const HomePage: React.FC = () => {
       if (!sessionId || !user?.username) return;
 
       try {
+<<<<<<< Updated upstream
         const res = await api(`/collaboration/collaboration/${sessionId}`);
         console.log(res);
+=======
+        const res = await fetch(
+          `${GATEWAY_URL}/collaboration/collaboration/${sessionId}`
+        );
+>>>>>>> Stashed changes
         if (!res.ok) {
           console.warn("Session fetch failed:", res.status);
           return;
